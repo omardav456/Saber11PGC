9e34a6e933fbb3b17368c7b8e38a365a9a985c05
Omar Quintero <omardav456@gmail.com>
2026-05-22 23:25:07 -0500

    feat: agrega la implementacion de JPA y consulta personalizada por área

---
+++ questions/src/main/java/com/saber11/questions/infraestructure/driver_adapters/jpa_repository/QuestionDataJpaRepository.java
@@ -0,0 +1,12 @@
+package com.saber11.questions.infraestructure.driver_adapters.jpa_repository;
+
+import com.saber11.questions.domain.model.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface QuestionDataJpaRepository extends JpaRepository<QuestionData,Long> {
+
+    Optional<QuestionData> findByArea(Area area);
+
+}
