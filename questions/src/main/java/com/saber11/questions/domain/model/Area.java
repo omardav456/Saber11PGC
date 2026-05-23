af7560f4b4fbc714aafb38f2ad1957870554b645
Omar Quintero <omardav456@gmail.com>
2026-05-22 22:59:33 -0500

    feat: agrega modelo de preguntas y enum de área de conocimiento

---
+++ questions/src/main/java/com/saber11/questions/domain/model/Area.java
@@ -0,0 +1,9 @@
+package com.saber11.questions.domain.model;
+
+public enum Area {
+    MATEMATICAS,
+    LECTURACRITICA,
+    SOCIALESYCIUDADANAS,
+    CIENCIASNATURALES,
+    INGLES
+}
---
+++ questions/src/main/java/com/saber11/questions/domain/model/Question.java
@@ -0,0 +1,15 @@
+package com.saber11.questions.domain.model;
+
+public class Question {
+    Long id;
+    Area area;
+    String  question;
+    String  answer;
+    String optionA;
+    String optionB;
+    String optionC;
+    String optionD;
+    String justification;
+
+
+}
