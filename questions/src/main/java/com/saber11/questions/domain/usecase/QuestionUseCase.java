e44e0c93cb1f8ef7449143a684b0eae21a96f6c0
Omar Quintero <omardav456@gmail.com>
2026-05-22 23:11:09 -0500

    feat: agrega caso de uso sin validaciones

---
+++ questions/src/main/java/com/saber11/questions/domain/usecase/QuestionUseCase.java
@@ -0,0 +1,35 @@
+package com.saber11.questions.domain.usecase;
+
+import com.saber11.questions.domain.model.Area;
+import com.saber11.questions.domain.model.Question;
+import com.saber11.questions.domain.model.gateway.QuestionGateway;
+
+import java.util.List;
+
+public class QuestionUseCase {
+
+    QuestionGateway questionGateway;
+
+    public Question createQuestion(Question question){
+        return questionGateway.createQuestion(question);
+    }
+
+    public Question findQuestionById(Long id){
+        return questionGateway.findQuestionById(id);
+    }
+
+    public List<Question> findQuestionsByArea(Area area){
+        return questionGateway.findQuestionsByArea(area);
+    }
+
+    public List<Question> findAllQuestions(){
+        return questionGateway.findAllQuestions();
+    }
+
+    Question updateQuestion(Question question){
+        return questionGateway.updateQuestion(question);
+    }
+    void deleteQuestionById(Long id){
+        questionGateway.deleteQuestionById(id);
+    }
+}
