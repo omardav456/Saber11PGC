c573ea68f590fde19300bd317e0aa26a3516679c
Omar Quintero <omardav456@gmail.com>
2026-05-26 11:59:55 -0500

    feat: Estructura del microservicio de simulacros

---
+++ exam/src/test/java/com/saber11/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saber11.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
