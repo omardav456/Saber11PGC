192df18676ae714c327a211edfca8ac6676a47df
PaulaMart57 <martinezmariapaula76@gmail.com>
2026-05-28 17:07:00 -0500

    feat:agrega clases de controller y usecaseconfig

--- notificationemail/src/main/java/com/saber11/notificationemail/application/config/UseCaseConfig.java
+++ notificationemail/src/main/java/com/saber11/notificationemail/application/config/UseCaseConfig.java
@@ -1,4 +1,15 @@
 package com.saber11.notificationemail.application.config;
 
+import com.saber11.notificationemail.domain.model.gateway.NotificationGateway;
+import com.saber11.notificationemail.domain.usecase.NotificationUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseConfig {
+    @Bean
+    public NotificationUseCase notificationUseCase(
+            NotificationGateway notificationGateway){
+        return new NotificationUseCase(notificationGateway);
+    }
 }
--- notificationemail/src/main/java/com/saber11/notificationemail/infraestructure/entry_points/NotificationController.java
+++ notificationemail/src/main/java/com/saber11/notificationemail/infraestructure/entry_points/NotificationController.java
@@ -1,1 +1,27 @@
+package com.saber11.notificationemail.infraestructure.entry_points;
+
+import com.saber11.notificationemail.domain.model.Notification;
+import com.saber11.notificationemail.domain.usecase.NotificationUseCase;
+
+import com.saber11.notificationemail.infraestructure.dto.NotificationRequest;
+import com.saber11.notificationemail.infraestructure.mapper.NotificationMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api/ecommerce/notification")
+@RequiredArgsConstructor
+
+public class NotificationController {
+    private final NotificationUseCase notificationUseCase;
+    private final NotificationMapper notificationMapper;
+    @PostMapping("/send")
+    public ResponseEntity<String> sendEmail(
+            @RequestBody NotificationRequest request) {
+        Notification notification = notificationMapper.toNotification(request);
+        String response = notificationUseCase.sendEmail(notification);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
 
