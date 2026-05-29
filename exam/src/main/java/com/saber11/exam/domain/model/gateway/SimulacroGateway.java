3cbb101a3c5eac67678cac00e767bc66c4de93f0
Omar Quintero <omardav456@gmail.com>
2026-05-29 15:57:55 -0500

    feat: agrega implementacion del puerto

--- exam/src/main/java/com/saber11/exam/domain/model/gateway/SimulacroGateway.java
+++ exam/src/main/java/com/saber11/exam/domain/model/gateway/SimulacroGateway.java
@@ -1,13 +1,14 @@
 package com.saber11.exam.domain.model.gateway;
 
 import com.saber11.exam.domain.model.Area;
+import com.saber11.exam.domain.model.Categoria;
 import com.saber11.exam.domain.model.Simulacro;
 
 import java.util.List;
 
 public interface SimulacroGateway {
     Simulacro createSimulacro(Simulacro sumulacro);
-    Simulacro getSimulacroByCategoria(Area area);
+    Simulacro getSimulacroByCategoria(Categoria categoria);
     void deleteSimulacro(Long id);
     Simulacro getSimulacroById(Long id);
     List<Simulacro> getAllSimulacro();
---
+++ exam/src/main/java/com/saber11/exam/infraestructure/driver_adapters/jpa_repository/SimulacroDataGatewayImp.java
@@ -0,0 +1,51 @@
+package com.saber11.exam.infraestructure.driver_adapters.jpa_repository;
+
+import com.saber11.exam.domain.model.Area;
+import com.saber11.exam.domain.model.Categoria;
+import com.saber11.exam.domain.model.Simulacro;
+import com.saber11.exam.domain.model.gateway.SimulacroGateway;
+import com.saber11.exam.infraestructure.mapper.MapperSimulacro;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class SimulacroDataGatewayImp implements SimulacroGateway {
+    private final SimulacroDataJpaRepository simulacroDataJpaRepository;
+    private final MapperSimulacro mapperSimulacro;
+
+
+    @Override
+    public Simulacro createSimulacro(Simulacro sumulacro) {
+        SimulacroData simulacroData = mapperSimulacro.toSimulacroData(sumulacro);
+        Simulacro simulacro= mapperSimulacro.toSimulacro(simulacroDataJpaRepository.save(simulacroData));
+        return simulacro;
+    }
+
+    @Override
+    public Simulacro getSimulacroByCategoria(Categoria categoria) {
+        SimulacroData simulacro = simulacroDataJpaRepository.findByCategoria(categoria).orElseThrow(() -> new RuntimeException("No existe"));
+        return mapperSimulacro.toSimulacro(simulacro);
+    }
+
+    @Override
+    public void deleteSimulacro(Long id) {
+        simulacroDataJpaRepository.deleteById(id);
+    }
+
+    @Override
+    public Simulacro getSimulacroById(Long id) {
+        SimulacroData simulacroData= simulacroDataJpaRepository.findById(id).orElseThrow(() -> new RuntimeException("No existe"));
+        return mapperSimulacro.toSimulacro(simulacroData);
+    }
+
+    @Override
+    public List<Simulacro> getAllSimulacro() {
+        List<SimulacroData> list = simulacroDataJpaRepository.findAll();
+        List<Simulacro> simulacros = list.stream().map(simulacroData -> mapperSimulacro.toSimulacro(simulacroData)).toList();
+        return simulacros;
+    }
+}
