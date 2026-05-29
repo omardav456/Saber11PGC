8c75577f3be80f17f699a5245647748fc6f4469b
Omar Quintero <omardav456@gmail.com>
2026-05-29 16:17:06 -0500

    feat: agrega dto y handler para manejo de excepciones

---
+++ exam/src/main/java/com/saber11/exam/infraestructure/entry_points/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.saber11.exam.infraestructure.entry_points.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ErrorResponse {
+
+    private String message;
+    private int code;
+    private LocalDateTime timestamp;
+
+}
---
+++ exam/src/main/java/com/saber11/exam/infraestructure/entry_points/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.saber11.exam.infraestructure.entry_points.handler;
+
+import com.saber11.exam.infraestructure.entry_points.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleException(RuntimeException ex){
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
